0af7b16a061eab80daa2dbd5687d6c6bdf8560ec
Joe Goggins <goggins@umn.edu>
2010-01-26 17:19:30 -0600

    Tweaked generator to accommodate string_ids=true. Added some stuff to initializer for those integrating with sanction_ui

--- test/app_root/db/migrate/010_create_roles.rb
+++ test/app_root/db/migrate/010_create_roles.rb
@@ -1,8 +1,20 @@
 class CreateRoles < ActiveRecord::Migration
   def self.up
     create_table :roles do |t|
-      t.belongs_to :principal, :polymorphic => true
-      t.belongs_to :permissionable, :polymorphic => true
+      # In many cases when integrating with legacy data, you
+      # need string principal_id and permissionable_id rather than
+      # integer's, to test this, test with:
+      #   rake test string_ids=true
+      if ENV['string_ids']
+        t.string :principal_id, :limit => 30
+        t.string :principal_type
+        t.string :permissionable_id, :limit => 30
+        t.string :permissionable_type
+      else
+        t.belongs_to :principal, :polymorphic => true
+        t.belongs_to :permissionable, :polymorphic => true
+      end
+      
       t.string     :name
       t.boolean    :global, :default => false
     end
